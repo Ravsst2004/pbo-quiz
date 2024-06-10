bd45869e9b5af07c0325ad857cad52c6db01290c
niahahaha <kartikaputrinia7@gmail.com>
2024-06-10 11:44:13 +0800

    Nambah sout

--- src/Read/Read.java
+++ src/Read/Read.java
@@ -4,5 +4,6 @@ public class Read {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
     System.out.println("Kocak gaming");
+    System.out.println("DEDE GAY");
   }
 }
