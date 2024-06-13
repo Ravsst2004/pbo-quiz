08e0a21310bfadf10a38372479d092e5a93b68ec
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-13 15:42:32 +0800

    Adding Read CSV file feature

--- src/Read/CSVReader.java
+++ src/Read/CSVReader.java
@@ -7,37 +7,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CSVReader {
-
-  private String filePath;
-  private String delimiter;
-
-  public CSVReader(String filePath, String delimiter) {
-    this.filePath = filePath;
-    this.delimiter = delimiter;
-  }
-
-  public List<String[]> read() {
-    List<String[]> data = new ArrayList<>();
+  public List<String[]> readCSV(String filePath) {
+    List<String[]> lines = new ArrayList<>();
     try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
       String line;
       while ((line = br.readLine()) != null) {
-        String[] values = line.split(delimiter);
-        data.add(values);
+        String[] data = line.split(",");
+        lines.add(data);
       }
     } catch (IOException e) {
-      e.printStackTrace();
+      throw new RuntimeException(e);
     }
-    return data;
-  }
-
-  public static void main(String[] args) {
-    CSVReader reader = new CSVReader("path/to/your/csvfile.csv", ",");
-    List<String[]> data = reader.read();
-    for (String[] row : data) {
-      for (String value : row) {
-        System.out.print(value + " ");
-      }
-      System.out.println();
-    }
+    return lines;
   }
 }
