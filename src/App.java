8b35ca590a5c81f7dfabd7148827259b42d19272
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-06-13 15:45:48 +0800

    -

--- src/App.java
+++ src/App.java
@@ -1,28 +1,5 @@
-import java.util.List;
-
-import Read.CSVReader;
-import Write.Write;
-
 public class App {
     public static void main(String[] args) {
-        // Write write = new Write();
-        // write.tulisSoal();
-        // write.writeCSV("CsvFile/quiz_data.csv");
-
-        CSVReader CSVreader = new CSVReader();
-        List<String[]> data = CSVreader.readCSV("CsvFile/question.csv");
-        int questionNumber = 1;
-        char optionLetter = 'A';
-
-        for (String[] questionData : data) {
-            System.out.println(questionNumber + ". " + questionData[0]);
-            for (int i = 1; i < questionData.length - 1; i++) {
-                System.out.println("    " + optionLetter + ". " + questionData[i]);
-                optionLetter++;
-            }
-            optionLetter = 'A';
-            questionNumber++;
-        }
-
+        System.out.println("Hello, World!");
     }
 }
